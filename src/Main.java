bb8d42e36ebb974d60643891e84a67136867897a
yura178500 <yura178500@yandex.ru>
2022-10-23 18:11:07 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int deliveryDistance = 30;
+        int deliveryDistance = 32;
+        double day = deliveryDistance / 20;
         if (deliveryDistance > 0 && deliveryDistance < 20) {
-            System.out.println("Потребуется дней: " +deliveryDistance + "  сутки на достваку");
+            System.out.println("Потребуется дней: " + day + "  сутки на достваку");
         }
         if (deliveryDistance > 20 && deliveryDistance < 60) {
-            System.out.println("Потребуется дней: " +deliveryDistance + "   + 2 суток на достваку");
+            System.out.println("Потребуется дней: " + day +"   + 2 суток на достваку");
         }
         if (deliveryDistance > 60 && deliveryDistance < 100) {
-            System.out.println("Потребуется дней: " +deliveryDistance + "   + 3 суток на достваку");
+            System.out.println("Потребуется дней: " + day + "   + 3 суток на достваку");
         } ;
-        int delivery = 100 - deliveryDistance;
+        double deliverDay = 5-day;
+        int delivery = (int) deliverDay;
         printdelivery(delivery);
     }
 }
